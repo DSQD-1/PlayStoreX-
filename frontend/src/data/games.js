de0caa8950bbaf52efc51f7e6788d8a64b30827e
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-18 11:37:49 +0300

    Create games.js

---
+++ frontend/src/data/games.js
@@ -0,0 +1,34 @@
+export const games = [
+  {
+    id: "roblox",
+    name: "Roblox",
+    icon: "🎮",
+    categories: [
+      "Robux",
+      "Подарочные карты",
+      "Донат",
+      "Аккаунты",
+      "Limiteds"
+    ]
+  },
+  {
+    id: "brawl-stars",
+    name: "Brawl Stars",
+    icon: "⭐",
+    categories: [
+      "Донат",
+      "Аккаунты",
+      "Скины"
+    ]
+  },
+  {
+    id: "standoff-2",
+    name: "Standoff 2",
+    icon: "⚡",
+    categories: [
+      "Голда",
+      "Скины",
+      "Аккаунты"
+    ]
+  }
+];
