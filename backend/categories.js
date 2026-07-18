9e4ddf602e7625134fa9e8cbc776b411f221b9a7
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-18 12:21:29 +0300

    Create categories.js

---
+++ backend/categories.js
@@ -0,0 +1,27 @@
+export const categories = [
+  "Робуксы",
+  "Подарочные карты",
+  "Донат робуксов (паки)",
+  "Аккаунты",
+
+  "Standoff 2",
+  "Brawl Stars",
+
+  "Adopt Me",
+  "Blox Fruits",
+  "Grow a Garden",
+  "Steal a Brainrot",
+  "99 Nights in the Forest",
+  "Murder Mystery 2",
+  "Pet Simulator 99",
+  "Anime Defenders",
+  "Anime Vanguards",
+  "The Strongest Battlegrounds",
+  "Jailbreak",
+  "Jujutsu Infinite",
+  "King Legacy",
+  "Fisch",
+  "Forsaken",
+
+  "Другие игры"
+];
