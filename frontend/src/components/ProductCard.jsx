9e7a5de249d8e25b99d595a95ced3c827eb41d88
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-18 14:59:50 +0300

    Update ProductCard.jsx

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,33 +1,49 @@
 export default function ProductCard({ product }) {
+
   return (
-    <div className="card">
+    <div className="product-card">
 
-      {product.image ? (
-        <img 
-          src={product.image}
-          alt={product.title}
-        />
-      ) : (
-        <div>
-          🎮
+      <div className="product-image">
+
+        {product.image ? (
+          <img
+            src={product.image}
+            alt={product.title}
+          />
+        ) : (
+          <div className="no-image">
+            🎮
+          </div>
+        )}
+
+      </div>
+
+
+      <div className="product-info">
+
+        <h3>
+          {product.title}
+        </h3>
+
+        <p className="game">
+          {product.game}
+        </p>
+
+
+        <div className="bottom">
+
+          <span className="price">
+            {product.price}₽
+          </span>
+
+          <button>
+            Купить
+          </button>
+
         </div>
-      )}
 
-      <h3>
-        {product.title}
-      </h3>
 
-      <p>
-        {product.game} • {product.category}
-      </p>
-
-      <strong>
-        {product.price} ₽
-      </strong>
-
-      <button>
-        Купить
-      </button>
+      </div>
 
     </div>
   );
