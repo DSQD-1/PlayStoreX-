08db1c960d8ef97b0533d49e57402150af1aa960
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-18 15:40:29 +0300

    Update telegram.js

--- frontend/src/telegram.js
+++ frontend/src/telegram.js
@@ -1,34 +1,12 @@
 import WebApp from "@twa-dev/sdk";
 
-
 export function initTelegram() {
-
 
   WebApp.ready();
 
-
-  WebApp.expand();
-
-
-
-  const user = WebApp.initDataUnsafe?.user;
-
-
-
-  console.log(
-    "Telegram user:",
-    user
-  );
-
-
-
   return {
-
-    user: user || null,
-
+    user: WebApp.initDataUnsafe?.user || null,
     colorScheme: WebApp.colorScheme
-
   };
-
 
 }
