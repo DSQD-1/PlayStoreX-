329fdd4fb812e9a9d4a892deb6ca5c8df1d92984
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-18 11:31:56 +0300

    Create telegram.js

---
+++ frontend/src/telegram.js
@@ -0,0 +1,10 @@
+import WebApp from "@twa-dev/sdk";
+
+export function initTelegram() {
+  WebApp.ready();
+
+  return {
+    user: WebApp.initDataUnsafe?.user || null,
+    colorScheme: WebApp.colorScheme,
+  };
+}
