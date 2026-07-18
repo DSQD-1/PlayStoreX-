17cd60e755625f87c9d4eeda1a2dbb45e4c351fa
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-18 12:11:48 +0300

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,65 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+let users = [];
+let products = [];
+let orders = [];
+
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "PlayStoreX API работает"
+  });
+});
+
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+
+app.post("/products", (req, res) => {
+
+  const product = {
+    id: Date.now(),
+    ...req.body
+  };
+
+  products.push(product);
+
+  res.json(product);
+
+});
+
+
+app.post("/orders", (req, res) => {
+
+  const order = {
+    id: Date.now(),
+    status: "waiting",
+    ...req.body
+  };
+
+  orders.push(order);
+
+  res.json(order);
+
+});
+
+
+app.listen(3000, () => {
+  console.log(
+    "PlayStoreX API запущен"
+  );
+});
