3e12c07a7a6fd6f42f2cce9a5ade54f1f695f72f
DSQD-1 <dsq_d_tg@bk.ru>
2026-07-18 11:42:16 +0300

    Create products.js

---
+++ frontend/src/data/products.js
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    id: 1,
+    game: "Roblox",
+    category: "Robux",
+    title: "1000 Robux",
+    price: 499,
+    image: "",
+    seller: "PlayStoreX"
+  },
+  {
+    id: 2,
+    game: "Brawl Stars",
+    category: "Донат",
+    title: "170 гемов",
+    price: 299,
+    image: "",
+    seller: "PlayStoreX"
+  },
+  {
+    id: 3,
+    game: "Standoff 2",
+    category: "Голда",
+    title: "1000 голды",
+    price: 199,
+    image: "",
+    seller: "PlayStoreX"
+  }
+];
